4ee79e5dd2537f745ed5dfc1e52dbee85917fd8b
perissology <perissology@protonmail.com>
2017-10-04 17:04:59 -0700

    add getState to milestone wrapper

--- js/LPPMilestone.js
+++ js/LPPMilestone.js
@@ -1,5 +1,34 @@
-const LPPMilestoneAbi = require("../build/LPPMilestone.sol").LPPMilestoneAbi;
-const LPPMilestoneByteCode = require("../build/LPPMilestone.sol").LPPMilestoneByteCode;
+const LPPMilestoneAbi = require('../build/LPPMilestone.sol').LPPMilestoneAbi;
+const LPPMilestoneByteCode = require('../build/LPPMilestone.sol').LPPMilestoneByteCode;
 const generateClass = require('eth-contract-class').default;
 
-module.exports = generateClass(LPPMilestoneAbi, LPPMilestoneByteCode);
+const LPPMilestone = generateClass(LPPMilestoneAbi, LPPMilestoneByteCode);
+
+LPPMilestone.prototype.getState = function () {
+  return Promise.all([
+      this.liquidPledging(),
+      this.idProject(),
+      this.maxAmount(),
+      this.cumulatedReceived(),
+      this.reviewer(),
+      this.recipient(),
+      this.newReviewer(),
+      this.newRecipient(),
+      this.accepted(),
+      this.canceled(),
+    ])
+    .then(results => ({
+      liquidPledging: results[0],
+      idProject: results[1],
+      maxAmount: results[2],
+      cumulatedReceived: results[3],
+      reviewer: results[4],
+      recipient: results[5],
+      newReviewer: results[6],
+      newRecipient: results[7],
+      accepted: results[8],
+      canceled: results[9],
+    }));
+};
+
+module.exports = LPPMilestone;
